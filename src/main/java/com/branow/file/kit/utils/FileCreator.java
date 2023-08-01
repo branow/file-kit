07267cc6e4eff4c798497f09db37c65add0fa64c
branow <bodnarorestdraw@gmail.com>
2023-08-01 18:45:29 +0300

    create class FileCreator and mathing junit test

---
+++ src/main/java/com/branow/file/kit/utils/FileCreator.java
@@ -0,0 +1,100 @@
+package com.branow.file.kit.utils;
+
+import com.branow.file.kit.io.RuntimeIOException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+
+/**
+ * The {@code FileCreator} is a class that lets create a file appropriately to gotten creation options.<br>
+ * CreateOptions: {@link CreateOption#DIRECTORY}, {@link CreateOption#NONEXISTENT_PARENT},
+ * {@link CreateOption#MAY_EXIST}
+ * */
+public class FileCreator {
+
+    /**
+     * The {@code CreateOption} describes possible file creation options.
+     * <ul>
+     *   <li>{@link CreateOption#DIRECTORY} : create a directory. If this option isn't added,
+     *   it creates a usual file.</li>
+     *   <li>{@link CreateOption#NONEXISTENT_PARENT} : create all nonexistent parents of the
+     *   file if they are.</li>
+     *   <li>{@link CreateOption#MAY_EXIST} : doesn't create new file or throw exception if
+     *   the file of gotten path already exists.</li>
+     * </ul>
+     * */
+    public enum CreateOption {
+        DIRECTORY, NONEXISTENT_PARENT, MAY_EXIST,
+    }
+
+    /**
+     * The method create an instance of {@code FileCreator} class according to gotten
+     * creation options.
+     * @param options are the file creation options according to which the {@code FileCreator}
+     * instance is adjusted.
+     * @return  an instance of {@code FileCreator} class.
+     * @see CreateOption
+     * */
+    public static FileCreator of(CreateOption... options) {
+        boolean isDirectory = false, canHasNonexistentParent = false, canBeExistent = false;
+        for (CreateOption option: options) {
+            switch (option) {
+                case DIRECTORY -> isDirectory = true;
+                case NONEXISTENT_PARENT -> canHasNonexistentParent = true;
+                case MAY_EXIST -> canBeExistent = true;
+            }
+        }
+        return new FileCreator(isDirectory, canHasNonexistentParent, canBeExistent);
+    }
+
+
+    private final boolean isDirectory;
+    private final boolean mayHasNonexistentParent;
+    private final boolean mayBeExistent;
+
+    /**
+     * @param isDirectory if it's {@code true}, it creates a directory, if {@code false} - a usual file
+     * @param canHasNonexistentParent if it's {@code true}, it creates all nonexistent parents if they are.
+     *                                If it's {@code false} and there are nonexistent parents, the exception
+     *                                is thrown
+     * @param canBeExistent if it's {@code true}, it doesn't create new file or throw exception if the file
+     *                      of gotten path already exists.
+     * */
+    public FileCreator(boolean isDirectory, boolean canHasNonexistentParent, boolean canBeExistent) {
+        this.isDirectory = isDirectory;
+        this.mayHasNonexistentParent = canHasNonexistentParent;
+        this.mayBeExistent = canBeExistent;
+    }
+
+    /**
+     * The method create a file by the gotten path according to gotten option throw the {@code constructor}.
+     * The method use method of class {@link Files}.
+     * @param path the path to the file to create
+     * @see Files#createFile(Path, FileAttribute[])
+     * @see Files#createDirectory(Path, FileAttribute[])
+     * @see Files#createDirectories(Path, FileAttribute[])
+     * */
+    public void create(Path path) {
+        if (Files.exists(path)) {
+            if (mayBeExistent)
+                return;
+            else
+                throw new IllegalArgumentException("Such file already exists: " + path);
+        }
+
+        try {
+            if (mayHasNonexistentParent)
+                Files.createDirectories(path.getParent());
+
+            if (isDirectory)
+                Files.createDirectory(path);
+            else
+                Files.createFile(path);
+        } catch (IOException e) {
+            throw new RuntimeIOException(e);
+        }
+    }
+
+}
